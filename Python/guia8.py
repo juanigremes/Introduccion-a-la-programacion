d4aa8d756cbce928ed91a102a04692e1fa7df138
juanigremes <juanigremes@gmail.com>
2024-05-31 17:25:32 -0300

    ejercicios guia 8

--- Python/guia8.py
+++ Python/guia8.py
@@ -1,7 +1,8 @@
 #Ej 1.1
 def contarLineas (nombreArchivo: str) -> int:
-    archivo = open(nombreArchivo)
+    archivo = open(nombreArchivo,'r')
     leoLineas = archivo.readlines()
+    archivo.close()
     return len(leoLineas)
 
 #print(contarLineas("miArchivo.txt"))
@@ -33,7 +34,7 @@ def armarPalabras (texto: str) -> list[str]:
 #print (armarPalabras("   hola    que   tal "))
 
 def existePalabra (palabra: str, nombreArchivo: str) -> bool:
-    archivo = open(nombreArchivo)
+    archivo = open(nombreArchivo,'r')
     leoArchivo = archivo.read()
     archivo.close()
     res : bool = (pertenece(armarPalabras(leoArchivo),palabra))
@@ -53,7 +54,7 @@ def contarApariciones (listaPalabras: list[str], palabra: str) -> int:
     return contador
 
 def cantidad_apariciones (nombreArchivo: str, palabra:str) -> int:
-    archivo = open(nombreArchivo)
+    archivo = open(nombreArchivo,'r')
     leoArchivo = archivo.read()
     archivo.close()
     res: int = contarApariciones(armarPalabras(leoArchivo),palabra)
@@ -62,10 +63,73 @@ def cantidad_apariciones (nombreArchivo: str, palabra:str) -> int:
 """
 print (cantidad_apariciones ("miArchivo.txt","hola"))
 print (cantidad_apariciones ("miArchivo.txt","lol"))
-print (cantidad_apariciones ("miArchivo.txt","hola"))
+print (cantidad_apariciones ("miArchivo.txt","siu"))
 print (cantidad_apariciones ("miArchivo.txt","capo"))
 print (cantidad_apariciones ("miArchivo.txt","teorema"))
 """
+
+#Ej 2
+def identificarPrimerCaracter(linea: list[str]) -> str:
+    primerCaracter: str = ""
+    indice: int = 0
+    while linea[indice] == ' ':
+        indice += 1
+    if linea[indice] == '#':
+        primerCaracter = '#'
+    else:
+        primerCaracter = str(linea[indice])
+    return primerCaracter
+
+def caracterValido (caracter: str) -> bool:
+    res: bool = True
+    if caracter == '#':
+        res = False
+    return res
+
+def clonar_sin_comentarios (nombreArchivo: str) -> str:
+    archivoOriginal = open(nombreArchivo,'r')
+    lineasArchivo = archivoOriginal.readlines()
+    archivoOriginal.close()
+    archivoSinComentarios: str = []
+    for linea in lineasArchivo :
+        if caracterValido(identificarPrimerCaracter(linea)):
+            archivoSinComentarios.append(linea)
+    archivoFinal: str = ""
+    for n in range (0,len(archivoSinComentarios),1):
+        archivoFinal = archivoFinal + archivoSinComentarios[n]
+    return archivoFinal
+
+#print (clonar_sin_comentarios("miArchivo.txt"))
+
+#Ej 3
+def invertirLineas (nombreArchivo: str) -> str:
+    archivoOriginal = open(nombreArchivo,'r')
+    lineasArchivo = archivoOriginal.readlines()
+    archivoOriginal.close()
+    archivoCasiInvertido: str = []
+    for linea in lineasArchivo:
+        lineaInvertida: str = ""
+        for n in range (0,len(linea),1):
+            lineaInvertida = linea[n] + lineaInvertida
+        archivoCasiInvertido.append(lineaInvertida)
+    archivoInvertido: str = ""
+    for linea in archivoCasiInvertido:
+        for n in range (0,len(archivoCasiInvertido),1):
+            archivoInvertido = archivoCasiInvertido[n] + archivoInvertido
+    return archivoInvertido
+    
+#print (invertirLineas("miArchivo.txt"))
+# SOLAMENTE TENIA QUE INVERTIR LAS LINEAS E INVERTI todo el TEXTO
+
+#Ej 4
+def agregarFraseAlFinal (nombreArchivo: str, frase: str):
+    archivo = open(nombreArchivo,'r+')
+    archivoConFrase = archivo.write("\n" + frase)  
+    archivo.close
+    return archivoConFrase
+
+print (agregarFraseAlFinal ("miArchivo.txt","frase agregada al final"))
+#ARREGLAR, NO ESTA TERMINADO
 
 # PILAS
 
